de356b1024b5c8f597a46d9932b8b3ee15de96ed
Daria-Churkina <darka.yaremenko@gmail.com>
2020-10-13 19:30:54 +0300

    исправляет изменение темы

--- goit-js-hw-10-food-service/src/js/theme-switch.js
+++ goit-js-hw-10-food-service/src/js/theme-switch.js
@@ -2,38 +2,23 @@ const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
-
-const refs = {
-  themeInput: document.querySelector('.js-switch'),
-  body: document.querySelector('body'),
-};
+const themeInput = document.querySelector('#theme-switch-toggle');
+const body = document.querySelector('body');
 
 const currentTheme = localStorage.getItem('theme');
 
-if (currentTheme === Theme.DARK) {
-  refs.themeInput.checked = true;
-  refs.body.classList.add(Theme.DARK);
+if (currentTheme === Theme.LIGHT) {
+  body.classList.add(Theme.LIGHT);
+} else {
+  body.classList.add(Theme.DARK);
+  themeInput.setAttribute('checked', true);
 }
 
-if (currentTheme !== Theme.DARK) {
-  refs.themeInput.checked = false;
-  refs.body.classList.add(Theme.LIGHT);
-}
-
-const handleChange = event => {
-  if (event.target.checked) {
-    refs.body.classList.remove(Theme.LIGHT);
-    refs.body.classList.add(Theme.DARK);
-
+themeInput.addEventListener('change', () => {
+  body.classList.toggle(Theme.DARK);
+  if (body.classList.contains(Theme.DARK)) {
     localStorage.setItem('theme', Theme.DARK);
-  }
-
-  if (!event.target.checked) {
-    refs.body.classList.remove(Theme.DARK);
-    refs.body.classList.add(Theme.LIGHT);
-
+  } else {
     localStorage.setItem('theme', Theme.LIGHT);
   }
-};
-
-refs.themeInput.addEventListener('change', handleChange);
+});
